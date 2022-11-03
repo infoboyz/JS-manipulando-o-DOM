cbf3d34371727c6af53e2771be0fb82a9c7316bd
infoboyz <jorge12sam@gmail.com>
2022-11-03 12:25:34 -0400

    Realizando o segundo desafio alura

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-document.getElementById('texto').innerText = "Ta fogo"
+const btNotas = document.querySelector('#calcular');
+const resultado = document.querySelector('.resultado');
+
+btNotas.addEventListener("click", () => {
+    resultado.innerHTML = "Fui clicado";
+})
