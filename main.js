8a7c35f1b2835b658a51e80977ec090949af1a3d
infoboyz <jorge12sam@gmail.com>
2022-11-03 19:17:02 -0400

    Realizando o terceiro desafio alura

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ const resultado = document.querySelector('.resultado');
 btNotas.addEventListener("click", () => {
     resultado.innerHTML = "Fui clicado";
 })
+
+var lista = ["Laranja", "Vermelho", "Branco", "Amarelo", "Rosa"]; 
+
+lista.splice(1,1);
+
+console.log(lista);
