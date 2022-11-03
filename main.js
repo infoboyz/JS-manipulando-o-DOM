3bf96270024e50b2373ef010cc0b96f4669198ad
infoboyz <jorge12sam@gmail.com>
2022-11-03 12:11:37 -0400

    Funcoes com JavaScript

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 const robotron = document.querySelector('#robotron');
 
-function dizOi () {
-    console.log('oi');
+function dizOi (nome) {
+    console.log("Oi " + nome);
+    console.log('Bem-vindo ao Robotron 2000');
 }
 
 // robotron.onclick = dizOi;
-robotron.addEventListener("click", dizOi)
+robotron.addEventListener("click", (evento) => {
+    console.log(evento);
+})
+
+dizOi("Pedro");
